d0bbed3ed0e6a9f7e999512f74376f8bec768436
Mithun Kumar <dharur.kumar@s.amity.edu>
2025-03-26 14:03:34 +0530

    edit case 1

--- txt.c
+++ txt.c
@@ -4,16 +4,15 @@ int main()
  switch;
 case 1:
 printf("B.tech.CSE)
-printf("Btech.CSE");
+printf("B.tech.CSE);
+printf("B.tech.CSE);
+printf("B.tech.CSE);
  case 2:
-printf("Btech AIML")
 printf("Btech AIML");
 break;
  case 3:
 printf("Btech ECE")
-printf("Btech ECE");
 break;
 case 4:
 printf("Btech MEC");
-printf("Btech MEC";
 break;
